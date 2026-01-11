5bf106cf6426bac65054e768d49922191164ed1c
gabcytn <cayetanogabriel03@gmail.com>
2026-01-11 15:30:17 +0800

    style(prettier): update formatting rules for code style

--- src/components/ModalTextInput.vue
+++ src/components/ModalTextInput.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
 const props = defineProps<{
-    label: string,
-    placeholder: string,
-    id: string,
-    type: string,
+  label: string;
+  placeholder: string;
+  id: string;
+  type: string;
 }>();
 
 const model = defineModel();
 </script>
 
 <template>
-    <div>
-        <label :for="props.id" class="block text-sm font-medium text-slate-300 pb-2">{{ props.label }}</label>
-        <input v-model="model" :id="props.id" :type="props.type" :placeholder="props.placeholder" required
-            class="w-full px-4 py-2.5 bg-slate-800/50 border border-slate-700 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 text-slate-100 placeholder-slate-500" />
-    </div>
+  <div>
+    <label
+      :for="props.id"
+      class="block text-sm font-medium text-slate-300 pb-2"
+      >{{ props.label }}</label
+    >
+    <input
+      v-model="model"
+      :id="props.id"
+      :type="props.type"
+      :placeholder="props.placeholder"
+      required
+      class="w-full px-4 py-2.5 bg-slate-800/50 border border-slate-700 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 text-slate-100 placeholder-slate-500"
+    />
+  </div>
 </template>
